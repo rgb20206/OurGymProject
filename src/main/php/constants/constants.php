93586f69eca468647dc5a00cac80eadc0929d61b
Dominic Reveglia (Student) <dominic.reveglia.2020@uni.strath.ac.uk>
2021-06-01 17:43:57 +0100

    reverted merge

--- src/main/php/constants/constants.php
+++ src/main/php/constants/constants.php
@@ -1,8 +1,1 @@
 <?php
-
-define('HOME', 'https://devweb2020.cis.strath.ac.uk/~yxb20181/OurGymProject/src/main/php/index.php');
-define('ABOUT', 'https://devweb2020.cis.strath.ac.uk/~rgb20206/OurGymProject/about.php');
-define('CONTACT', 'https://devweb2020.cis.strath.ac.uk/~yxb20181/OurGymProject/src/main/php/contact.php');
-define('PLANS', 'https://devweb2020.cis.strath.ac.uk/~rgb20206/OurGymProject/plans.php');
-define('FACILITIES', 'https://devweb2020.cis.strath.ac.uk/~rgb20206/OurGymProject/facilities.php');
-define('CLASSES', 'https://devweb2020.cis.strath.ac.uk/~rgb20206/OurGymProject/classes.php');
