3695753bf8b897c7580a4e227d5c5b227ec69f2e
Jenna Lawrence (Student) <jenna.lawrence.2020@uni.strath.ac.uk>
2021-06-02 19:27:40 +0100

    Tidied up code of about page and style sheet

--- src/main/php/about.php
+++ src/main/php/about.php
@@ -19,46 +19,46 @@ include("constants/constants.php");
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<section class="about">
+<section class="section-about">
     <?php include("universal-header.php");  ?>
-    <div class="about-page">
-        <h1 class="about-title">About OurGym</h1>
-        <br>
-        <p class="about-statement">From low cost flexible memberships and 24 hour opening times, to quality gym equipment and classes included, there are loads of reasons why OurGym is one of the top rated in the UK.
-
-            Our mission is to help people achieve their fitness goals. Come and join us today.</p>
-        <br>
-        <br>
-        <p class="about-fun-facts">Learn a bit more about us in these 5 fun facts below!</p>
-        <br>
-        <ul class="bullet-point">
-        <li>    We were voted number 1 gym in the UK in 2014</li>
-            <br>
-        <li>    We were the first gym in the UK to introduce anti-gravity yoga</li>
-            <br>
-        <li>    We offer over 20 different classes, more than 80% of leading gyms in England</li>
-            <br>
-        <li>    Our most popular class is our 'pop spin class' in which we have over 200 weekly participants </li>
-            <br>
-        <li>    We offer online classes as part of your membership</li>
-        </ul>
-    </div>
-</section>
-</body>
-
-<!--Footer-->
-    <footer>
-        <div class="footer-text">
-            <h3>OurGym</h3>
-            <p>Follow us on any of our social media platforms for the latest updates and great deals!</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+    <div class="about">
+        <div>
+            <h1>About OurGym</h1>
+            <p>From low cost flexible memberships and 24 hour opening times,
+                <br>to quality gym equipment and classes included, there are loads of reasons why
+                <br>OurGym is one of the top rated in the UK.
+                <br>Our mission is to help people achieve their fitness goals. Come and join us today.
+            </p>
+        </div>
+        <div>
+            <h3>Learn a bit more about us in these 5 fun facts below!</h3>
+            <ul class="bullet-point">
+                <li>We were voted number 1 gym in the UK in 2014</li>
+                <li>We were the first gym in the UK to introduce anti-gravity yoga</li>
+                <li>We offer over 20 different classes, more than 80% of leading gyms in England</li>
+                <li>Our most popular class is our 'pop spin class' in which we have over 200 weekly participants </li>
+                <li>We offer online classes as part of your membership</li>
             </ul>
         </div>
-        <div class="copyright">
-            <p>copyright &copy;2020 Jenna, Arran, Dom :)</p>
-        </div>
-    </footer>
+    </div>
+</section>
+
+<!--Footer-->
+<footer>
+    <div class="footer-text">
+        <h3>OurGym</h3>
+        <p>Follow us on any of our social media platforms for the latest updates and great deals!</p>
+        <ul class="socials">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+        </ul>
+    </div>
+    <div class="copyright">
+        <p>copyright &copy;2020 Jenna, Arran, Dom :)</p>
+    </div>
+</footer>
+
+</body>
+</html>
